f749be97dba3668e9ef05eeb6a921bc46bc36585
wosslr <wosslr@163.com>
2015-11-25 11:43:18 +0800

    v1

--- housefinance/admin.py
+++ housefinance/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import User, Account, AccountingDocumentHeader, AccountingDocumentItem
 # Register your models here.
+
+
+class AccountingDocumentAdmin(admin.ModelAdmin):
+    fields = ['creation_date', 'creator', 'comment']
+
+
+admin.site.register(AccountingDocumentHeader, AccountingDocumentAdmin)
 admin.site.register(User)
 admin.site.register(Account)
-admin.site.register(AccountingDocumentHeader)
-admin.site.register(AccountingDocumentItem)

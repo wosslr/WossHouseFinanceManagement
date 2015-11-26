135c75f2f1a5947c63152f10540198bffa964c44
Ray Lv <ray.lv@sap.com>
2015-11-26 18:40:03 +0800

    v7

--- housefinance/views.py
+++ housefinance/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from  django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("Hello you are in the index")
+
+def detail(request, accountingDocumentID):
+    response = "You're looking at the detail of accounting document %s."
+    return HttpResponse(response % accountingDocumentID)
+

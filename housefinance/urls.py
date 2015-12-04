082159af1fe9a612baf3b0f139d29e088254d576
wosslr <wosslr@163.com>
2015-12-05 03:32:14 +0800

    V6

--- housefinance/urls.py
+++ housefinance/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail')
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^chart_spend_data/$', views.chart_spend_data, name='chart_spend_data'),
+    url(r'^chart_spend/$', views.ChartSpendView.as_view(), name='chart_spend')
 ]
--- housefinance/views.py
+++ housefinance/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.views import generic
 
-from .models import AccountingDocumentHeader
+import json
+
+from .models import AccountingDocumentHeader, AccountingDocumentItem
 
 # Create your views here.
 
@@ -20,3 +22,15 @@ class DetailView(generic.DetailView):
     model = AccountingDocumentHeader
     context_object_name = 'account_doc'
     template_name = 'housefinance/detail.html'
+
+
+class ChartSpendView(generic.TemplateView):
+    template_name = 'housefinance/chart_spend.html'
+
+
+def chart_spend_data(request):
+    acc_doc_items = AccountingDocumentItem.objects.all()
+    result = []
+    for acc_doc_item in acc_doc_items:
+        result.append(json.dumps({'y': acc_doc_item.amount.__float__()}))
+    return HttpResponse(result)

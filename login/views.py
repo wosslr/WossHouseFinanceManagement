322112e1524a68beb4e2884771e3a8c5ab833204
wosslr <wosslr@163.com>
2015-12-28 23:18:53 +0800

    V6

--- login/views.py
+++ login/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, render_to_response
 from django import forms
 from django.http import HttpResponseRedirect
-from django.contrib.auth.admin import User
+from django.contrib.auth.models import User
+from django.template.context_processors import csrf
 
 
 # Create your views here.
@@ -18,11 +19,15 @@ def login(request):
             username = uf.cleaned_data['username']
             password = uf.cleaned_data['password']
             # 获取的表单数据与数据库进行比较
+            print(username, password)
             user = User.objects.filter(username__exact=username, password__exact=password)
+            print(user)
             if user:
                 return render_to_response('housefinance/index.html', {'username': username})
             else:
                 return HttpResponseRedirect('/login/')
     else:
         uf = UserForm()
-    return render_to_response(template_name='login/login.html', context={'uf': uf})
+    context = {'uf': uf}
+    context.update(csrf(request))
+    return render_to_response(template_name='login/login.html', context=context)

c322d0075ae0059e82ababa9fb06a116d26ec7ab
wosslr <wosslr@163.com>
2016-01-18 23:06:25 +0800

    V6

--- housefinance/templatetags/filters.py
+++ housefinance/templatetags/filters.py
@@ -5,4 +5,13 @@ register = template.Library()
 
 @register.filter(name='set_class')
 def set_class(value, arg):
-    return value.as_widget(attrs={'class': arg})
+    # return value.as_widget(attrs={'class': arg})
+    value.field.widget.attrs['class'] = arg
+    return value
+
+
+@register.filter(name='set_pholder')
+def set_pholder(value, arg):
+    # return value.as_widget(attrs={'class': arg})
+    value.field.widget.attrs['placeholder'] = arg
+    return value
